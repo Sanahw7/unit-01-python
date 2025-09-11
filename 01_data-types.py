51a195d2b89a7871374e985ae66e6bcc0b1a8953
Sanah White <sanahwhite7@gmail.com>
2025-09-11 10:50:30 -0400

    Work

---
+++ 01_data-types.py
@@ -0,0 +1,63 @@
+"""
+TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+my_age= 17.01 #Made a variable
+
+age= int(my_age) #Made new variable and put old variable int 
+
+print(my_age) #Printed orignal variable and converted integer
+
+"""
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+
+
+
+"""
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+first_num= 98 #Made variable of integer
+
+second_num= 10.1 #made variable of float
+
+print(first_num+second_num) #Printed addition problem
+
+
+
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+{ "grapes":6 #gave fruit name and value
+
+"plums":9 #gave fruit name and value
+
+print(x["plums"]) #printed the value of plums
+
+}
+
+"""
+TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
+
+"""
+TASK 6:
+
+Create a list of your favorite subjects (as strings). 
+Use the join() function to combine all subjects into a single string separated by commas.
+Then create another version using " - " as the separator.
+Print both the original list and both joined strings.
