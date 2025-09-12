6714637d74daba24832910453f0dc4604074191b
Sanah White <sanahwhite7@gmail.com>
2025-09-12 10:50:20 -0400

    work3

---
+++ 02_formatting.py
@@ -0,0 +1,50 @@
+"""
+TASK 1:
+Write code that checks if a user entered the correct password.
+The password should not be case sensitive
+
+"""
+my_pass= House
+
+
+
+"""
+TASK 2:
+Write code that checks if a user inputs an empty string
+If the string is empty, print "invalid" otherwise print "valid"
+"""
+
+
+"""
+TASK 3:
+
+Write a program that will replace the word "cat" with the word "dog"
+It should replace all occurances regardless of captilization 
+"""
+animal = "Cat" #made variable for cat
+
+new_animal = animal. replace(Cat, Dog) #made new variable for replacement
+
+print(new_animal) #used print function for dog
+
+
+"""
+TASK 4:
+
+Write a program that takes a person's name and age as input and prints it
+"""
+age = 17 #made a variable for my age
+
+name = "Sanah is" + str(age) #made variable for name and made the string connected to age
+
+print(name) #should print Sanah is 17
+
+
+"""
+TASK 5:
+
+Write a program that takes in two floats, and prints the quotient
+The result should be rounded to the nearest tenth (1 decimal place)
+"""
+
+num =
