21586111b2b0b756154efc55534b8d16713e49fd
Sanah White <sanahwhite7@gmail.com>
2025-09-15 10:46:16 -0400

    Lists

---
+++ 03_list_blitz.py
@@ -0,0 +1,74 @@
+"""""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+my_lst= ['Apples', 'plums', 'grapes', 'oranges'] #Made a list of fruits also a variable
+
+print(my_lst) # printed my variable
+
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+House=[] #Made an empty list
+House.append("Door") #added door to my list
+print(House) #printed out new variable
+
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+Bedroom["Bed, TV"] #Made a list
+Bedroom.remove("TV") #removed TV
+print(Bedroom) #printed new list with only bed
+
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+my_list=["Car", "Money", "House"] #created a list
+my_list[0]= "Cat" #changed the value of index 0
+print(my_list) #printed new list without old value
+
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+my_l= ["deftones"] #made a list 
+my_l.append("fleshwater") #added to list
+my_l.append("whirr") #added to list
+my_l.append("the smiths") #added to list
+print(my_l) #printed the new list with added rock bands
+
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+my_lists= ["silent hill", "dmc","elden ring"] #made a list of games
+del my_lists[2] #deleted specific index, elden ring
+print(my_lists) #printed new list without elden ring 
+
+
+"""
+Task 7: Slicing lists
+Create a new variable equal to the first 2 items of your list
+Then print out the new variable
+"""
+fruits =["grapes", "durian", "peach", "apple"] #made a list
+print(fruits[0:2]) #only selected 2 so grapes and peach
+
+"""
+Task 8: Extend a List
+Extend the list with the elements of another list. Print 
+the updated list.
+"""
+adding_a= ["Sleeping", "In"] #made a list
+adding_b= ["My", "Bed"] #A second list
+adding_together= adding_a + adding_b #added those 2 lists so they'll be combined 
+print(adding_together) #Should print Sleeping In My Bed
+
