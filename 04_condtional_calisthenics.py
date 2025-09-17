50e8967624f64626e9f942edc76b744632638a09
Sanah White <sanahwhite7@gmail.com>
2025-09-17 10:50:53 -0400

    resubmit_ifstatement

--- 04_condtional_calisthenics.py
+++ 04_condtional_calisthenics.py
@@ -3,9 +3,9 @@ Exercise 1:
 Check if an integer is even and greater than 10.
 Return True if both conditions are met, False otherwise.
 '''
-if 22 > 10:
-    print("True")
-else:
+if 22 > 10:  #made an if statement 
+    print("True") #What's supposed to print if the if statement is correct
+else:       #alternative answer
     print("False")
 
 '''
@@ -13,14 +13,14 @@ Exercise 2:
 Determine the ticket price based on age and student status.
 Price is $10 if under 18 or a student, $20 otherwise.
 '''
-Print("Are you a student?")
-Answer1= input( "yes or no")
-Print("What's your age?")
+Print("Are you a student?") #Prompted user with a question 
+Answer1= input( "yes or no") #gave them 2 choices
+Print("What's your age?") #Asked another question 
 Answer2= input()
-if Answer1== "yes" or Answer2 < 18:
-    print("Ticket price is $10") 
+if Answer1== "yes" or Answer2 < 18: #Made an if statement 
+    print("Ticket price is $10") #the outcome if they're less than 18 and/or a student
 else:
-    print("Ticket price is $20")
+    print("Ticket price is $20") #Other option if they're an adult
 
 '''
 Exercise 3:
@@ -28,13 +28,13 @@ Prompt the user to enter a fruit name. Check if the fruit is in the list.
 If it is, print "Yes, that fruit is in the list." 
 If it's not, print "No, that fruit is not in the list."
 '''
-print("Enter Apple.")
-Fruit1= ["Apple", "Apple", "Apple"]
-print("Apple" in Fruit1)
-if "Apple" in Fruit1:
-    print("Yes, that fruit is in the list.")
+print("Enter Apple.") #Gave user a message
+Fruit1= ["Apple", "Apple", "Apple"] #Created list of fruits
+print("Apple" in Fruit1) #Checking to see if Apple is in the list
+if "Apple" in Fruit1: #if statement
+    print("Yes, that fruit is in the list.") #Outcome 1 if the user puts Apple
 else: 
-    print("No, that fruit is not in the list.")
+    print("No, that fruit is not in the list.") #Outcome 2 if user doesn't put apple 
 
 
 
@@ -51,7 +51,8 @@ Exercise 5:
 Calculate the cost of shipping for an online order based on the order weight and destination zone. 
 The shipping cost is $5 per kilogram for Zone A and $7 per kilogram for Zone B. 
 If the order weight is less than 0 kg, return an error message.
-'''
+''''
+
 
 '''
 Exercise 6:
