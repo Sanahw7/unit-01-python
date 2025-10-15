3954607b0268defe90b8f82a86c2300325cb005c
Sanahw7 <sanahwhite7@gmail.com>
2025-10-14 21:09:34 -0400

    Datetime_done

--- 10_dubious_dates.py
+++ 10_dubious_dates.py
@@ -15,28 +15,28 @@ Exercise 2:
 Write a Python program that prints the current date and time using the datetime module.
 Using the strftime function format the date in standard U.S. date format (MM/DD/YYYY)
 """
-from datetime import datetime
+from datetime import datetime 
 
-my_datee = datetime.now()
+my_datee = datetime.now() #created a variable using the datetime.now module so it'd find the current date
 
-the_string = my_datee.strftime("%m/%d/%Y")
+the_string = my_datee.strftime("%m/%d/%Y") #the strftime was used because it formats how you want your time to look capitalization matters
 
-print(the_string)
+print(the_string) #It should print out the date with "/"
 
 """
 Exercise 3:
 Using the strptime function, convert two strings into dates.
 Then find the difference in days between the two.
 """
-my_string1 = "09/07/2008"
+my_string1 = "09/07/2008" #The first string 
 
-my_date = datetime.strptime(my_string1, "%m/%d/%Y")
+my_date = datetime.strptime(my_string1, "%m/%d/%Y") #formatted the date using strptime with a new variable
 
-my_string2 = "05/27/2005"
+my_string2 = "05/27/2005" #the second date 
 
-my_date2 = datetime.strp(my_string2, %m/%d/%Y)
+my_date2 = datetime.strptime(my_string2, "%m/%d/%Y") #formatted the date 
 
-print(my_date-my_date2)
+print(my_date-my_date2) #this prints out the difference between the days of the dates
 
 
 """
@@ -45,8 +45,8 @@ Write a program that asks the user for their birthdate and calculates their curr
 age using the datetime module.
 """
 
-print("What's your birthdate")
+print("What's your birthdate?") #The user is prompted with a question 
 
-answer1 = input()
+answer1 = input() #They will have to enter their birthdate in mm-dd-YYYY format
 
-my_date = datetime.strptime(answer1, "%m/%d/%Y")
+my_date = datetime.strptime(answer1, "%m/%d/%Y") 
