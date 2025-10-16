363d88fdce16d321b931d8fb8ee0f42c098f1e22
Sanahw7 <sanahwhite7@gmail.com>
2025-10-16 00:26:03 -0400

    phonebook_done

--- 11_contact_dictionary.py
+++ 11_contact_dictionary.py
@@ -1,36 +1,36 @@
 print("Contact Menu:")
 
-menu= {}
+menu= {} #Made an empty dictionary for the users answer
 
-while True:
+while True: #used a while statement to it would go on forever
     
-    question1 = input('would you like to "Add contact", "Delete contact ", "List contacts" or "Exit"')
+    question1 = input('would you like to "Add contact", "Delete contact ", "List contacts" or "Exit"') #The user has to input one of the 4 choices
     
-    if question1 == "Add contact":
+    if question1 == "Add contact": #For the first choice 
         
-        name = input("enter name")
-        phone = int(input("Enter phone number"))
-        menu[name] = phone
-        print("Contact successfully added")
+        name = input("enter name") #Enter the name
+        phone = int(input("Enter phone number")) #put in the number and I used the integer fucntion so it allows numbers
+        menu[name] = phone #put the key into the the brackets
+        print("Contact successfully added") #The result is the contact name and phone is added and you're prompted with this message
         
-    if question1 == "Delete contact":
+    if question1 == "Delete contact": #The second choice 
         name2 = input("Enter name")
         phone2 = int(input("Enter phone number"))
-        del menu[name2]
+        del menu[name2] #put the key in the variable so it will delete it
         print("Contact successfully deleted")
         
         
     if question1 == "List contacts":
         
-        for x in menu:
-            print(x)
-            print(menu[x])
+        for x in menu: 
+            print(x) #prints the string
+            print(menu[x]) #prints the number. So both are printed line by line listing them vertically
 
             
             
     if question1 == "Exit":
             
-            break
+            break #pauses the code
     
         
        
