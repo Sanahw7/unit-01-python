0fab636c3d7d8490ac2dd71b28c258c6bf390e9a
Sanah White <sanahwhite7@gmail.com>
2025-10-16 10:50:21 -0400

    half_donefunction

---
+++ 12_functions.py
@@ -0,0 +1,68 @@
+"""
+Task 1: Greeting
+Write a function that takes a name as a 
+agrument and prints a greeting message like "Hello, [name]!".
+"""
+
+def my_function(name):
+    """
+    
+    """
+    print("Hello," + name )
+my_function("Sanah!")
+
+"""
+Task 2: Square of a Number
+Write a function that takes an integer as an argument and returns its square.
+"""
+def num_b (a):
+    """
+    
+    """
+    
+    return a**2
+x = num_b (9)
+
+
+
+
+"""
+Task 3: Even or Odd
+Write a function that takes a number as a argument that 
+returns `True` if the number is even, and `False` otherwise.
+"""
+def num_A (d):
+    
+    return num_A % 2 ==0
+    
+
+
+
+
+"""
+Task 4: Area of a Rectangle
+Write a function that takes the length and width of a rectangle and returns its area.
+"""
+
+
+"""
+Task 5: Celsius to Fahrenheit
+Write a function that takes a temperature in Celsius and returns 
+the equivalent temperature in Fahrenheit using the correct formula
+"""
+
+
+"""
+Task 6: Average of Numbers
+Write a function that takes a list of numbers as an argument
+and returns the average (mean) of those numbers.
+"""
+
+"""
+Task 7: Total Calculator
+Create a function that has arguments for the price and quantity of something, 
+and returns the total.
+
+Your function should also optionally accept a 3rd argument for discount as a float, 
+and return the discounted if provided.
+"""
