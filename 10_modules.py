bb8c152201a2e7d6b099a244eb934fb424492362
Sanahw7 <sanahwhite7@gmail.com>
2025-10-17 19:31:13 -0400

    Modules_done

--- 10_modules.py
+++ 10_modules.py
@@ -17,11 +17,9 @@ Create a new directory called "test_folder" in the current directory.
 Then print a list of all files and directories in the current directory.
 """
 
-var2 = os.listdir()
+var2 = os.listdir() #made a variable that holds the os and the command that list the files and current directories
 
 print()
-
-
 
 """
 Task 3 (os module):
@@ -29,14 +27,14 @@ Write a program that checks if a directory called "data" exists in the current
 working directory. If it doesn't exist, create it. If it does exist, print 
 "Directory already exists."
 """
-directory_P = "Data"
+directory_P = "Data" 
 
 
-if os.path.isdir("Data"):
+if os.path.isdir("Data"): #used a conditional so they'll be able to tell if the directory exist or not
     print("Directory already exists")
     
 else:
-    print("Directory doesn't exist")
+    print("Directory doesn't exist") #This is what's printed if it doesn't exist
     
     
 
@@ -48,7 +46,7 @@ If it exists, print its path. If it doesn't exist, print "File not found."
 """
 file_p = "config.txt"
 
-if os.path.exists(file_p)
+if os.path.exists(file_p) #this tests if file_p exist or not
     print("")
     
 else:
@@ -58,7 +56,7 @@ else:
 Task 5 (sys module):
 Write a program that prints the Python version you are currently using.
 """
-print(sys.version)
+print(sys.version) #used sys module and the .version sees what version of python you're using 
 
 """
 Task 6 (sys module):
@@ -67,4 +65,4 @@ Write a program that prints the platform your Python interpreter is running on
 "Linux", "Windows", "MacOS"
 """
 
-print(sys.platform)
+print(sys.platform) #.platform sees what version interpreter you're using 
