e6efdbb1767b9fa059181eea64224cd6b9a8bc43
Sanah White <sanahwhite7@gmail.com>
2025-10-28 10:44:54 -0400

    trippy_testing

---
+++ 15_trippy_testing.py
@@ -0,0 +1,110 @@
+"""
+Exercise 1: Divide
+"""
+def divide(a, b): #def statement for 2 numbers for division
+  """Divides two numbers, handling potential division by zero.
+
+  Args:
+    a: The numerator.
+    b: The denominator.
+
+  Returns:
+    The quotient, or None if b is zero.
+  """
+
+  if b == 0: #the demnominater can't equal zero because your result will be nothing 
+    return None
+  else:
+    return a / b
+  
+assert divide(10,2)== 5 #this is to check if the statement is true
+
+"""
+Exercise 2: Factorial
+"""
+def factorial(n):
+  """Calculates the factorial of a non-negative integer.
+
+  Args:
+    n: A non-negative integer.
+
+  Returns:
+    The factorial of n.
+  """
+
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+  
+assert factorial(3) == 6
+
+"""
+Exercise 3: String Reverse
+"""
+def reverse_string(string):
+  """Reverses a given string.
+
+  Args:
+    string: A string to be reversed.
+
+  Returns:
+    The reversed string.
+  """
+
+  reversed_string = ""
+  for char in string:
+    reversed_string = char + reversed_string
+  return reversed_string
+
+assert reverse_string("hello") == "olleh"
+assert reverse_string("house") == "esuoh"
+
+
+
+"""
+Exercise 4: Fibonacci
+"""
+def fibonacci(n):
+  """Generates the nth Fibonacci number.
+
+  Args:
+    n: The index of the Fibonacci number to calculate.
+
+  Returns:
+    The nth Fibonacci number.
+  """
+
+  if n <= 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+assert fibonacci(3) == 2 #These give you the answer of what each assert would be 
+assert fibonacci(6) == 1
+assert fibonacci(9) == 34
+
+"""
+Exercise 5: Email Validation
+"""
+
+import re
+
+def is_valid_email(email):
+  """Determines whether email is valid or not
+
+  Args:
+    email: The email to validate
+
+  Returns:
+    Boolean value if email is valid
+  """
+  email_regex = r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+"
+  return re.match(email_regex, email) is not None
+
+assert is_valid_email("house@gmail.com") == True 
+
+  
+
